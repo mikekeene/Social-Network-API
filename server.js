b2e9495590882fb5b72c5df6bd733f6495b407fb
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-03-13 22:35:40 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-//app.use(require('./routes'));
+app.use(require('./routes'));
 
 const PORT = process.env.PORT || 3001;
 
