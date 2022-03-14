b948dbe30c1b130e72f12ada29cbc91ee2efc1e3
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-03-13 22:06:57 -0500

    Update thought-routes.js

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought, 
+    deleteThought,
+    createReaction,
+    deleteReaction
+} = require('../../controllers/thought-controller');
+
+router
+    .route('/')
+    .get(getAllThoughts);
+
+router
+    .route('/:id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+router
+    .route('/:userId')
+    .post(createThought);
+router
+    .route('/:thoughtId/reactions')
+    .post(createReaction);
+
+router
+    .route('/:thoughtId/reactions/:reactionId')
+    .delete(deleteReaction);
+
+module.exports = router;
